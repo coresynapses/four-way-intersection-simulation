3dda58cabff5baa54cff56ebf6ee168c4ba5e64d
011-zr <011-zr@tuta.io>
2025-04-14 16:15:39 -0500

    Project submission

--- main.c
+++ main.c
@@ -155,49 +155,34 @@ void SystemClock_Config(void) {
     RCC_OscInitTypeDef RCC_OscInitStruct = {0};
     RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-    /** Configure the main internal regulator output voltage
-    */
-    // __HAL_PWR_VOLTAGESCALECONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-    /** Initializes the RCC Oscillators according to the specified parameters
-    * in the RCC_OscInitTypeDef structure.
-    */
+    // Configure the main internal regulator output voltage
     RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
     RCC_OscInitStruct.HSIState = RCC_HSI_ON;
     RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-    // RCC_OscInitStruct.PLL.PLLState = RCC_PLL_OFF;
-    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-        Error_Handler();
-    }
-    /** Initializes the CPU, AHB and APB buses clocks
-    */
+
+    // Initializes the CPU, AHB and APB buses clocks
     RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1;
     RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
     RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
     RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
-        Error_Handler();
-    }
 }
 
 int main(void) {
-    // Reset of all peripherals, Initializes the Flash interface and the Systick.
+    // Initialize the System
     HAL_Init();
-
-    // Configure the system clock
     SystemClock_Config();
-
-    // Initialize all configured peripherals
     LED_GPIO_Init();
 
+    // Create the intersection
     Queue queues[NUM_APPROACHES];
     initializeQueue(&queues[0]); // North-South queue
     initializeQueue(&queues[1]); // East-West queue
 
+    // Initialize variables
     uint32_t time = 0;
     int randomNum = 0;
 
-    printf("Two-way Intersection Traffic Simulation\n");
+    printf("Four-way Intersection Traffic Simulation\n");
 
     while (1) {
         // Car Arrivals
